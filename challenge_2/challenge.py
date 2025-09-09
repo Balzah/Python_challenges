59177bdf1b836541a8d3f8c68aacccf45ec344d6
Balzah <b.bajcsay@gmail.com>
2025-09-09 13:36:19 +0200

    add solution challenge 2

--- challenge_2/challenge.py
+++ challenge_2/challenge.py
@@ -1,2 +1,13 @@
 #CHALLENGE 2
 #FIND THE CHARACTER AT A SPECIFIC INDEX
+
+
+myString = "Python" # P y t h o n
+i = 6              # 0 1 2 3 4 5
+
+if len(myString) == 0:
+     print("String is empty.")
+elif i< len(myString):
+     print(myString[i])
+else:
+     print("Out of range.")
