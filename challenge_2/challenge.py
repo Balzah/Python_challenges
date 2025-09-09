dee6f3b3f6a3c9e810fe42f2d3ed553b46effe6a
Balzah <b.bajcsay@gmail.com>
2025-09-09 13:48:58 +0200

    make solution for challenge 2 testable

--- challenge_2/challenge.py
+++ challenge_2/challenge.py
@@ -2,12 +2,11 @@
 #FIND THE CHARACTER AT A SPECIFIC INDEX
 
 
-myString = "Python" # P y t h o n
-i = 6              # 0 1 2 3 4 5
+def char_at_index(s: str, i: int) -> str:
 
-if len(myString) == 0:
-     print("String is empty.")
-elif i< len(myString):
-     print(myString[i])
-else:
-     print("Out of range.")
+    if len(s) == 0:
+        return "String is empty."
+    elif i < len(s):
+        return s[i]
+    else:
+        return "Out of range."
